5e5e0d25c9acb8870dfc8b6dfb083af23083423a
Saron Bhoopathy <saron@Macaroni.local>
2021-04-06 14:38:04 -0400

    added comments

--- project_function_template.m
+++ project_function_template.m
@@ -1,17 +1,17 @@
-replace this text with your function definition line
+% replace this text with your function definition line
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % ENGR 132 
 % Program Description 
-replace this text with your program description as a comment
+% replace this text with your program description as a comment
 %
 % Function Call
-replace this text with a comment that states the function call
+% replace this text with a comment that states the function call
 %
 % Input Arguments
-replace this text with a commented list of the input arguments
+% replace this text with a commented list of the input arguments
 %
 % Output Arguments
-replace this text with a commented list of the output arguments
+% replace this text with a commented list of the output arguments
 %
 % Assignment Information
 %   Assignment:     M2, Problem #
@@ -21,7 +21,7 @@ replace this text with a commented list of the output arguments
 %     [] We worked with one or more peers but our collaboration
 %        maintained academic integrity.
 %     Peers we worked with: Name, login@purdue [repeat for each]
-did you complete the assignment information? delete this line if yes
+% did you complete the assignment information? delete this line if yes
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% ____________________
