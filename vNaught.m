4248b51c664ff4b51b965848a825f54c948b616a
Saron Bhoopathy <sbhoopat@purdue.edu>
2021-04-14 03:08:39 -0400

    created calculations for vnaught

--- vNaught.m
+++ vNaught.m
@@ -29,37 +29,85 @@
 
 %% ____________________
 %% INITIALIZATION
+% readmatrix to create a matrix of data file
 rawData = readmatrix('Data_nextGen_KEtesting_allresults.csv');
 
+% substrate concentration values [uM]
 subConc = rawData(1,2:11);
+% time values [s]
 time = rawData(3:7486,1);
 
+% separation of test data for different enzymes
+% duplicates are first separated into 1 and 2
+% then the average of the data is found
+
+% test data for enzyme A
 enzymeA1 = rawData(3:7486,2:11);
 enzymeA2 = rawData(3:7486,12:21);
 enzymeA = (enzymeA1+enzymeA2)./2;
-
+% test data for enzyme B
 enzymeB1 = rawData(3:7486,22:31);
 enzymeB2 = rawData(3:7486,32:41);
 enzymeB = (enzymeB1+enzymeB2)./2;
-
+% test data for enzyme C
 enzymeC1 = rawData(3:7486,42:51);
 enzymeC2 = rawData(3:7486,52:61);
 enzymeC = (enzymeC1+enzymeC2)./2;
-
+% test data for enzyme D
 enzymeD1 = rawData(3:7486,62:71);
 enzymeD2 = rawData(3:7486,72:81);
 enzymeD = (enzymeD1+enzymeD2)./2;
-
+% test data for enzyme E
 enzymeE1 = rawData(3:7486,82:91);
 enzymeE2 = rawData(3:7486,92:101);
 enzymeE = (enzymeE1+enzymeE2)./2;
 
+vNaughtA = zeros(1,10);
+vNaughtB = zeros(1,10);
+vNaughtC = zeros(1,10);
+vNaughtD = zeros(1,10);
+vNaughtE = zeros(1,10);
 %% ____________________
 %% CALCULATIONS
 
+% Calculating v_naught_sub_i i.e. the initial reaction rate at different
+% substrate concentrations are each enzyme
+% A linear regression model is generated for 0-20 s of the reaction to
+% smoothen potential noise in the data. Now, v_naught is calculated using
+% the slope of the linear regression model.
+% Enzyme A
+for substrateLoop = 1:10
+    coeffs = polyfit(time(1:21),enzymeA(1:21,substrateLoop),1);
+    vNaughtA(1,substrateLoop) = coeffs(1);
+end
+% Enzyme B
+for substrateLoop = 1:10
+    coeffs = polyfit(time(1:21),enzymeB(1:21,substrateLoop),1);
+    vNaughtB(1,substrateLoop) = coeffs(1);
+end
+% Enzyme C
+for substrateLoop = 1:10
+    coeffs = polyfit(time(1:21),enzymeC(1:21,substrateLoop),1);
+    vNaughtC(1,substrateLoop) = coeffs(1);
+end
+% Enzyme D
+for substrateLoop = 1:10
+    coeffs = polyfit(time(1:21),enzymeD(1:21,substrateLoop),1);
+    vNaughtD(1,substrateLoop) = coeffs(1);
+end
+% Enzyme E
+for substrateLoop = 1:10
+    coeffs = polyfit(time(1:21),enzymeE(1:21,substrateLoop),1);
+    vNaughtE(1,substrateLoop) = coeffs(1);
+end
 
 %% ____________________
 %% FORMATTED TEXT/FIGURE DISPLAYS
+
+% Product Concentration [uM] vs. time [s] graphs for all 5 enzymes
+% This will help in identifying the slope to determine initial reaction
+% rate
+% Enzyme A
 figure(1)
 plot(time,enzymeA(:,1))
 xlabel('time [s]')
@@ -74,7 +122,7 @@ end
 lgd = legend('S1: 3.75','S2: 7.5','S3: 15','S4: 30','S5: 65','S6: 125','S7: 250','S8: 500','S9: 1000', 'S10: 2000');
 lgd.Title.String = 'Substrate Concentration [uM]';
 hold off
-
+% Enzyme B
 figure(2)
 plot(time,enzymeB(:,1))
 xlabel('time [s]')
@@ -89,7 +137,7 @@ end
 lgd = legend('S1: 3.75','S2: 7.5','S3: 15','S4: 30','S5: 65','S6: 125','S7: 250','S8: 500','S9: 1000', 'S10: 2000');
 lgd.Title.String = 'Substrate Concentration [uM]';
 hold off
-
+% Enzyme C
 figure(3)
 plot(time,enzymeC(:,1))
 xlabel('time [s]')
@@ -104,7 +152,7 @@ end
 lgd = legend('S1: 3.75','S2: 7.5','S3: 15','S4: 30','S5: 65','S6: 125','S7: 250','S8: 500','S9: 1000', 'S10: 2000');
 lgd.Title.String = 'Substrate Concentration [uM]';
 hold off
-
+% Enzyme D
 figure(4)
 plot(time,enzymeD(:,1))
 xlabel('time [s]')
@@ -119,7 +167,7 @@ end
 lgd = legend('S1: 3.75','S2: 7.5','S3: 15','S4: 30','S5: 65','S6: 125','S7: 250','S8: 500','S9: 1000', 'S10: 2000');
 lgd.Title.String = 'Substrate Concentration [uM]';
 hold off
-
+% Enzyme E
 figure(5)
 plot(time,enzymeE(:,1))
 xlabel('time [s]')
